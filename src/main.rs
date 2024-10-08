a0bf4bb8f51188f872f0c5feb719fe95963aa197
TusharSync <tushar.synchronizedcodelab@gmail.com>
2024-10-08 19:23:10 +0530

    Updated thecode

--- src/main.rs
+++ src/main.rs
@@ -355,55 +355,124 @@
 
 
 // Tuples can be used as function arguments and as return values.
-fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    // `let` can be used to bind the members of a tuple to variables.
-    let (int_param, bool_param) = pair;
+// fn reverse(pair: (i32, bool)) -> (bool, i32) {
+//     // `let` can be used to bind the members of a tuple to variables.
+//     let (int_param, bool_param) = pair;
 
-    (bool_param, int_param)
-}
+//     (bool_param, int_param)
+// }
 
-// The following struct is for the activity.
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
+// // The following struct is for the activity.
+// #[derive(Debug)]
+// struct Matrix(f32, f32, f32, f32);
+
+// fn main() {
+//     // A tuple with a bunch of different types.
+//     let long_tuple: (u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, char, bool) = (1u8, 2u16, 3u32, 4u64,
+//                       -1i8, -2i16, -3i32, -4i64,
+//                       0.1f32, 0.2f64,
+//                       'a', true);
+
+//     // Values can be extracted from the tuple using tuple indexing.
+//     println!("Long tuple first value: {}", long_tuple.0);
+//     println!("Long tuple second value: {}", long_tuple.1);
+
+//     // Tuples can be tuple members.
+//     let tuple_of_tuples: ((u8, u16, u32), (u64, i8), i16) = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+//     // Tuples are printable.
+//     println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+//     // But long Tuples (more than 12 elements) cannot be printed.
+//     //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+//     //println!("Too long tuple: {:?}", too_long_tuple);
+//     // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+//     let pair = (1, true);
+//     println!("Pair is {:?}", pair);
+
+//     println!("The reversed pair is {:?}", reverse(pair));
+
+//     // To create one element tuples, the comma is required to tell them apart
+//     // from a literal surrounded by parentheses.
+//     println!("One element tuple: {:?}", (5u32,));
+//     println!("Just an integer: {:?}", (5u32));
+
+//     // Tuples can be destructured to create bindings.
+//     let tuple: (i32, &str, f64, bool) = (1, "hello", 4.5, true);
+
+//     let (a, b, c, d) = tuple;
+//     println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+//     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+//     println!("{:?}", matrix);
+// }
+
+
+
+
+// use std::mem;
+
+// // This function borrows a slice.
+// fn analyze_slice(slice: &[i32]) {
+//     println!("First element of the slice: {}", slice[0]);
+//     println!("The slice has {} elements", slice.len());
+// }
 
 fn main() {
-    // A tuple with a bunch of different types.
-    let long_tuple: (u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, char, bool) = (1u8, 2u16, 3u32, 4u64,
-                      -1i8, -2i16, -3i32, -4i64,
-                      0.1f32, 0.2f64,
-                      'a', true);
+    // // Fixed-size array (type signature is superfluous).
+    // let xs: [i32; 5] = [1, 2, 3, 4, 5];
 
-    // Values can be extracted from the tuple using tuple indexing.
-    println!("Long tuple first value: {}", long_tuple.0);
-    println!("Long tuple second value: {}", long_tuple.1);
+    // // All elements can be initialized to the same value.
+    // let ys: [i32; 500] = [0; 500];
+    // // Indexing starts at 0.
+    // println!("First element of the array: {}", xs[0]);
+    // println!("Second element of the array: {}", xs[1]);
 
-    // Tuples can be tuple members.
-    let tuple_of_tuples: ((u8, u16, u32), (u64, i8), i16) = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    // // `len` returns the count of elements in the array.
+    // println!("Number of elements in array: {}", xs.len());
 
-    // Tuples are printable.
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    // // Arrays are stack allocated.
+    // println!("Array occupies {} bytes", mem::size_of_val(&xs));
 
-    // But long Tuples (more than 12 elements) cannot be printed.
-    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    //println!("Too long tuple: {:?}", too_long_tuple);
-    // TODO ^ Uncomment the above 2 lines to see the compiler error
+    // // Arrays can be automatically borrowed as slices.
+    // println!("Borrow the whole array as a slice.");
+    // analyze_slice(&xs);
 
-    let pair = (1, true);
-    println!("Pair is {:?}", pair);
+    // // Slices can point to a section of an array.
+    // // They are of the form [starting_index..ending_index].
+    // // `starting_index` is the first position in the slice.
+    // // `ending_index` is one more than the last position in the slice.
+    // println!("Borrow a section of the array as a slice.");
+    // analyze_slice(&ys[1 .. 4]);
 
-    println!("The reversed pair is {:?}", reverse(pair));
+    // // Example of empty slice `&[]`:
+    // let empty_array: [u32; 0] = [];
+    // assert_eq!(&empty_array, &[]);
+    // assert_eq!(&empty_array, &[][..]); // Same but more verbose
 
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses.
-    println!("One element tuple: {:?}", (5u32,));
-    println!("Just an integer: {:?}", (5u32));
+    // // Arrays can be safely accessed using `.get`, which returns an
+    // // `Option`. This can be matched as shown below, or used with
+    // // `.expect()` if you would like the program to exit with a nice
+    // // message instead of happily continue.
+    // for i in 0..xs.len() + 1 { // Oops, one element too far!
+    //     match xs.get(i) {
+    //         Some(xval) => println!("{}: {}", i, xval),
+    //         None => println!("Slow down! {} is too far!", i),
+    //     }
+    // }
 
-    // Tuples can be destructured to create bindings.
-    let tuple = (1, "hello", 4.5, true);
+    // Out of bound indexing on array causes compile time error.
+    //println!("{}", xs[5]);
+    // Out of bound indexing on slice causes runtime error.
+    //println!("{}", xs[..][5]);
 
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+    let array: [i32; 5] = [1, 2, 3, 4, 5];
+    let slice_from_array: &[i32] = &array[1..4]; // Slices elements 2, 3, and 4
+    println!("{:?}", slice_from_array); // Output: [2, 3, 4]
 
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
+    let vec: Vec<i32> = vec![10, 20, 30, 40, 50];
+    let slice_from_vec: &[i32] = &vec[2..5]; // Slices elements 30, 40, and 50
+    println!("{:?}", slice_from_vec); // Output: [30, 40, 50]
+
 }
